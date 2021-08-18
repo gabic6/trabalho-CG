731bcb8e080a2f2af4f7067bbbd10b1b8f53b63b
Thiago Almeida <thiago9864@hotmail.com>
2021-08-18 16:46:13 -0300

    Plano da cidade feito

--- T3/cidade.js
+++ T3/cidade.js
@@ -168,8 +168,10 @@ scene.add(plane);
 
 ////////////// Aqui entra o código da cidade /////////////////////
 
-var material_asfalto = new THREE.MeshLambertMaterial({ color: '#333333', side: THREE.DoubleSide });
-var material_meio_fio = new THREE.MeshLambertMaterial({ color: '#cccccc', side: THREE.DoubleSide });
+var material_asfalto = new THREE.MeshLambertMaterial({ color: '#333333'});
+var material_meio_fio = new THREE.MeshLambertMaterial({ color: '#cccccc' });
+var material_grama = new THREE.MeshLambertMaterial({ color: '#22FF00' });
+var material_fronteira_cidade = new THREE.MeshLambertMaterial({ color: '#33FF33' });
 
 var cidadeHolder = new THREE.Object3D();
 
@@ -199,6 +201,63 @@ function criaMeioFio(x, z, width, height, material){
     return box;
 }
 
+const predio1 = (x, z) => {
+    var material = new THREE.MeshLambertMaterial({ color: '#83BAD6'});
+
+    const depth = 100 * Math.random() + 10;
+    const width = 30;
+    const height = 30;
+    const boxGeometry = new THREE.BoxGeometry( width, depth, height );
+    const box = new THREE.Mesh( boxGeometry, material );
+    box.castShadow = true;
+    box.receiveShadow = true;
+
+    // Conversão de coordenadas do draw.io pro threejs
+    //box.rotateX(degreesToRadians(-90));
+    box.position.set(x + width/2.0, depth/2.0, z + height/2.0);
+
+    return box; 
+}
+
+const listaPredios = [
+    predio1,
+    predio1
+];
+
+const posicoesXZPredios = [
+    {x: -205, z: -236},
+    {x: -140, z: -236},
+    {x: -80, z: -236},
+    {x: 95, z: -236},
+    {x: -230, z: -125},
+    {x: -155, z: -125},
+    {x: -60, z: -150},
+    {x: -60, z: -95},
+    {x: 37.5, z: -125},
+    {x: 132.5, z: -125},
+    {x: -230, z: -40},
+    {x: -150, z: -15},
+    {x: -60, z: -15},
+    {x: 37.5, z: -40},
+    {x: 37.5, z: 15},
+    {x: 107.5, z: -40},
+    {x: 195, z: -5},
+    {x: 107.5, z: 30.6},
+    {x: 37.5, z: 90},
+    {x: 37.5, z: 176.7},
+    {x: 102.5, z: 175},
+    {x: 195, z: 176.7},
+    {x: 107.5, z: 90},
+    {x: 40, z: 280},
+    {x: -80, z: 280},
+    {x: -205, z: 280},
+    {x: -60, z: 175},
+    {x: -140, z: 175},
+    {x: -230, z: 130},
+    {x: 195, z: 109}
+]
+
+console.log("Num Predios:", posicoesXZPredios.length);
 function criarCidade(){
 
     //Ruas
@@ -211,12 +270,79 @@ function criarCidade(){
     cidadeHolder.add(criaPlano(20,-170,180,10, material_asfalto));
     cidadeHolder.add(criaPlano(20,-170,180,10, material_asfalto));
     cidadeHolder.add(criaPlano(95,-60,175,10, material_asfalto));
+    cidadeHolder.add(criaPlano(260,-50,10,290, material_asfalto));
+    cidadeHolder.add(criaPlano(150,-50,10,290, material_asfalto));
+    cidadeHolder.add(criaPlano(95,0,55,10, material_asfalto));
+    cidadeHolder.add(criaPlano(160,70,100,10, material_asfalto));
+    cidadeHolder.add(criaPlano(85,140,65,10, material_asfalto));
+    cidadeHolder.add(criaPlano(20,50,65,10, material_asfalto));
+    cidadeHolder.add(criaPlano(20,230,130,10, material_asfalto));
+    cidadeHolder.add(criaPlano(160,230,100,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-250,-280,250,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-250,-170,250,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-260,-280,10,640, material_asfalto));
+    cidadeHolder.add(criaPlano(-100,-160,10,100, material_asfalto));
+    cidadeHolder.add(criaPlano(-90,-110,90,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-170,-60,170,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-180,-60,10,290, material_asfalto));
+    cidadeHolder.add(criaPlano(-100,-50,10,110, material_asfalto));
+    cidadeHolder.add(criaPlano(-170,50,70,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-90,50,90,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-250,50,70,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-170,140,170,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-250,230,250,10, material_asfalto));
+    cidadeHolder.add(criaPlano(-130,240,10,110, material_asfalto));
+    cidadeHolder.add(criaPlano(-250,350,250,10, material_asfalto));
 
     // Meios fio
     cidadeHolder.add(criaMeioFio(20,-50,65,50, material_meio_fio));
     cidadeHolder.add(criaMeioFio(20,-160,65,100, material_meio_fio));
     cidadeHolder.add(criaMeioFio(95,-160,105,100, material_meio_fio));
     cidadeHolder.add(criaMeioFio(20,-270,180,100, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(95,-50,55,50, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(160,-50,100,120, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(20,10,65,40, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(95,10,55,130, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(160,80,100,150, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(20,60,65,170, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(85,150,65,80, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-250,-270,250,100, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-90,-160,90,50, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-90,-100,90,40, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-180,-160,80,100, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-250,-160,70,210, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-170,-50,70,100, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-90,-50,90,100, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-250,60,70,170, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-170,60,170,80, material_grama));
+    cidadeHolder.add(criaMeioFio(-170,150,170,80, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-250,240,120,110, material_meio_fio));
+    cidadeHolder.add(criaMeioFio(-120,240,120,110, material_meio_fio));
+
+    //Fronteiras da cidade
+    cidadeHolder.add(criaPlano(270,-310,90,700, material_fronteira_cidade));
+    cidadeHolder.add(criaPlano(210,-310,60,250, material_fronteira_cidade));
+    cidadeHolder.add(criaPlano(15,-310,195,30, material_fronteira_cidade));
+    cidadeHolder.add(criaPlano(-260,-310,265,30, material_fronteira_cidade));
+    cidadeHolder.add(criaPlano(-340,-310,80,700, material_fronteira_cidade));
+    cidadeHolder.add(criaPlano(-260,360,280,30, material_fronteira_cidade));
+    cidadeHolder.add(criaPlano(20,240,250,150, material_fronteira_cidade));
+
+    //Rua até o aeroporto
+    cidadeHolder.add(criaPlano(5,-475,10,196, material_asfalto));
+    cidadeHolder.add(criaPlano(-560,-485,575,10, material_asfalto));
+
+    //Aeroporto
+    cidadeHolder.add(criaPlano(-670,-530,110,100, material_meio_fio));
+    cidadeHolder.add(criaPlano(-700,-575,30,600, material_asfalto));
+
+    // Posiciona os predios
+    for(var i=0; i<posicoesXZPredios.length; i++){
+        var posPredio = posicoesXZPredios[i];
+        var predioInd = Math.floor((listaPredios.length-1) * Math.random());
+        var funcPredio = listaPredios[predioInd];
+        cidadeHolder.add(funcPredio(posPredio.x, posPredio.z));
+    }
 
     // Posiciona o holder um pouco mais alto que o plano pra não dar conflito
     cidadeHolder.position.set(0, 1.0, 0);
